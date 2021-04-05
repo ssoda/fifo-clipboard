fe87149de58f93497b0198a54476c9b2117e2696
ssoda <demaciasoda@gmail.com>
2021-04-05 22:37:29 +0800

    Show fifo list length on icon badge

--- background.js
+++ background.js
@@ -4,3 +4,11 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender) {
+    if (request.updateFifoListCount != undefined) {
+      chrome.action.setBadgeText({tabId: sender.tab.id, text: `${request.updateFifoListCount}`});
+    }
+  }
+);
--- content-script.js
+++ content-script.js
@@ -6,10 +6,12 @@ document.addEventListener('copy', (event) => {
   var copiedContent = event.target.value || event.target.textContent;
   if (copiedContent !== "") {
     fifoList.push(copiedContent);
+    chrome.runtime.sendMessage({updateFifoListCount: fifoList.length});
   }
 });
 
 document.addEventListener('paste', (event) => {
   event.preventDefault();
   document.execCommand('insertText', false, fifoList.shift());
+  chrome.runtime.sendMessage({updateFifoListCount: fifoList.length});
 });
